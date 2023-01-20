12f007d0054adbc67d72bf9cba3290c7f216cfcf
Brit Sovic <bsovic@tulane.edu>
2023-01-19 20:47:29 -0500

    updated queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,45 @@
 const Participant = require("../models/Participant");
+const User = require("../models/User");
 
 const resolvers = {
-    // Get All Participants By Descending Date Entered
+    
     Query: {
-        getParticipants: async () => {
+      // Get All Participants By Descending Date Entered
+      getParticipants: async () => {
         return Participant.find().sort({ entered_at: -1 });
+      },
+
+      // Get All Participants By First Name Entered
+      participantByName: async (parent, { first_name }) => {
+        const params = first_name ? { first_name } : {};
+        return Participant.find(params);
+      },
+
+      // Get All Participants By User Who Entered
+      participantByEnter: async (parent, { entered_by }) => {
+        const params = entered_by ? { entered_by } : {};
+        return Participant.find(params);
+      },
+
+      // Get Participants By Study
+      participantByStudy: async (parent, { study }) => {
+        Participant.find(params);
       }
-    }
+  },
+
+  Mutation: {
+      // Add User
+      addUser: async () => {
+        const user = await User.create(args);
+
+        return user;
+      },
+
+      // Add Participant
+      addParticipant: async () => {
+
+      }
+  }
   };
   
   module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,13 +8,43 @@ const typeDefs = gql`
         last_name: String
         dob: String
         digits: Int
+        site: String
+        study: String
         zipcode: Int
         entered_by: String
         entered_at: String
     }
-
+    type Question {
+        _id: ID
+        q_body: String
+    }
+    type Study {
+        _id: ID
+        study_name: String
+        study_year: Int
+    }
+    type User {
+        id: ID
+        user_fn: String
+        user_ln: String
+        user_email: String
+        user_password: String
+    }
+    type Site {
+        _id: ID
+        site_name: String
+        site_email: String
+        site_coordinator: String
+    }
     type Query {
         getParticipants: [Participant]
+        participantByName(first_name: String!): [Participant]
+        participantByEnter(entered_by: String!): [Participant]
+        participantByStudy(study: String!): [Participant]
+    }
+    type Mutation {
+        addUser(user_fn: String!, user_ln: String!, user_email: String!, user_password: String!): User
+        addParticipant(first_name: String!, last_name: String!, dob: String!, digits: Int!, site: String!, study: String!, zipcode: String!, entered_by: String!): Participant
     }
 `;
 
