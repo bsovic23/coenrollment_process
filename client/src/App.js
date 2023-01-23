c63237ec0330ca392b741495d1f12c3bc0063781
Brit Sovic <bsovic@tulane.edu>
2023-01-23 17:23:07 -0500

    updated structure to begin components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// Import Page/Tab components here
+/*
+
+
+*/
+
+// Import Re-used pieces here
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <section>
+    <header>
+      < Header />
+      < Navbar />
+    </header>
+
+    <body>
+
+    </body>
+
+    <footer>
+      < Footer />
+    </footer>
+  </section>
   );
 }
 
